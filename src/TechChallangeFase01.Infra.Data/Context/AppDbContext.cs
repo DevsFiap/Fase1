e4166518abb928e9b083c3d08c9807f622b34541
Leo Campos <leopcampos85@gmail.com>
2024-10-05 10:27:35 -0300

    Upload do projeto

---
+++ src/TechChallangeFase01.Infra.Data/Context/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TechChallangeFase01.Domain.Entities;
+
+namespace TechChallangeFase01.Infra.Data.Context;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {}
+
+    public DbSet<Contato> Contatos { get; set; }
+}
