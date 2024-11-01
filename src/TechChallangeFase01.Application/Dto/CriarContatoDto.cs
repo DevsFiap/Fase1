7bf4f7566d443daeabc306dd542273e8108bcc48
alexandrephausmann <alexandrephausmann@hotmail.com>
2024-11-01 19:18:50 -0300

    Adicionando validações

--- src/TechChallangeFase01.Application/Dto/CriarContatoDto.cs
+++ src/TechChallangeFase01.Application/Dto/CriarContatoDto.cs
@@ -7,12 +7,14 @@ namespace TechChallangeFase01.Application.Dto;
 public class CriarContatoDto
 {
     [Required(ErrorMessage = "O nome é obrigatório.")]
+    [MinLength(8,ErrorMessage ="O nome deve ter no minimo 8 letras")]
     public string Nome { get; set; }
 
     [Required(ErrorMessage = "O telefone é obrigatório.")]
     [RegularExpression(@"^\d{10,11}$", ErrorMessage = "O telefone deve ter entre 10 e 11 dígitos, sem formatação.")]
     public string Telefone { get; set; }
 
+    [Required(ErrorMessage = "O E-Mail é obrigatório.")]
     [EmailAddress(ErrorMessage = "E-mail inválido.")]
     public string Email { get; set; }
 
